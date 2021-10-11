c96ae9aa045840aa1475c9c0bd8d0dfce0b638e1
octokami <78549633+octokami@users.noreply.github.com>
2021-10-11 09:42:34 +0200

    Changed to Ridge regression model

--- assignment_1/training-cp/resources/model_trainer.py
+++ assignment_1/training-cp/resources/model_trainer.py
@@ -12,28 +12,52 @@ import statsmodels.api as sm
 
 
 def train(dataset):
-    # split into input (X) and output (Y) variables
-    X = dataset[:, 0:8]
-    Y = dataset[:, 8]
-    # define model
-    ##model = Sequential()
-    ##model.add(Dense(12, input_dim=8, activation='relu'))
-    ##model.add(Dense(8, activation='relu'))
-    ##model.add(Dense(1, activation='sigmoid'))
+    # We drop Latitude and Longitude from the set as they do little to serve as predictors for price.
+    # To prevent overfitting the model, no further variables will be dropped.
 
-    model = smf.ols('SoldPrice ~ LstPrice', data=houses)
+    X = dataset[['Acres', 'Deck', 'GaragCap', 'Patio', 'PkgSpacs', 'Taxes', 'TotBed', 'TotBth', 'TotSqf']]
+    y = dataset['SoldPrice']
+    X = sm.add_constant(X)
 
-    ## compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # Fit the model
-    model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
-    # evaluate the model
-    scores = model.evaluate(X, Y, verbose=0)
-    print(model.metrics_names)
-    text_out = {
-        "accuracy:": scores[1],
-        "loss": scores[0],
-    }
+    # We now wiull use scikitlearn's train_test_split to split our set into a training and a testing set for our
+    # X and y. I will do this in a way to prepare for a 5-fold cross validation. 
+    # once this is done, we use te train data to train our model and use the test data to see how well our prediction fits
+    # the test data.
+    # We generate a prediction
+
+    # We shall use R_squared as a metric of how well the model fits the test data.
+
+    r2_vals = []
+
+    for i in range(10) :
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+        model = sm.OLS(y_train, X_train).fit()
+        model_pred = model.predict(X_test)
+        r2_vals.append(r2(y_test, model_pred))
+
+    print('Mean R-Squared over 10 runs: ' + str(np.mean(r2_vals))) 
+    print("")
+    text_out = {model.summary()}
+
+    # Of all variables used in the model, two stand out to be particularly good for price prediction being Taxes and TotSqf
+    # with both their standard error being ~7 and ~11 respectively. they are followed by PkgSpacs, having a standard 
+    # error of ~3728. All other variables have a standard error that ios at least almost twice the standard error of 
+    # PkgSpacs.
+
+    # Using 5-fold cross validation, we tend to average around 0.8 for our R-Squared value.
+    # While close and fairly impressive by itself, it is not accurate enough to beat the R-Squared score
+    # of List Price vs SoldPrice at 0.991
+
+    ridge_reg = Ridge(alpha=10, fit_intercept=True)
+    ridge_reg.fit(X_train, y_train)
+
+    cols = ['const','Acres', 'Deck', 'GaragCap', 'Patio', 'PkgSpacs', 'Taxes', 'TotBed', 'TotBth', 'TotSqf']
+    print("Ridge regression model:\n {}+ {}^T . X".format(ridge_reg.intercept_, ridge_reg.coef_))
+    pd.Series(ridge_reg.coef_.flatten(), index=cols)
+
+    # We can see that as a result of the regularization, the coeffiecients have normalized to lower values.
+
     # Saving model in a given location provided as an env. variable
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
