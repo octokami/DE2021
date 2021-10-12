2eef76cd595d3973804286cd1dadb45bd12cbacb
octokami <78549633+octokami@users.noreply.github.com>
2021-10-12 22:26:24 +0200

    datapp_api

--- assignment_1/training-cp/app.py
+++ assignment_1/training-cp/app.py
@@ -13,13 +13,13 @@ app.config["DEBUG"] = True
 
 @app.route('/training-cp/model', methods=['POST'])
 def train_models():
-    db_api = os.environ['TRAININGDB_API']
+    datapp_api = os.environ['TRAININGDB_API']
     # Make a GET request to training db service to retrieve the training data/features.
-    r = requests.get(db_api)
+    r = requests.get(datapp_api)
     j = r.json()
     df = pd.DataFrame.from_dict(j)
     resp = model_trainer.train(df)
     return resp
 
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5006)
