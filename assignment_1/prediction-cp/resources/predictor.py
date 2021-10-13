c42ca4d879eb888e8e6aa3df19e0f8b59a6916a1
octokami <78549633+octokami@users.noreply.github.com>
2021-10-13 18:04:19 +0200

    Update predictor.py

--- assignment_1/prediction-cp/resources/predictor.py
+++ assignment_1/prediction-cp/resources/predictor.py
@@ -14,12 +14,12 @@ def predict(dataset):
         #model = load_model(file_path)
         model = pickle.load(open(file_path, 'rb'))
         #val_set2 = dataset.copy()
-        result = model.predict(dataset)
+        model_pred = model.predict(dataset)
         #y_classes = result.argmax(axis=-1)
         #val_set2['class'] = y_classes.tolist()
         #dic = result.to_dict(orient='records')
-        result.tolist()
-        text_out = {dic}
-        return json.dumps({result}), 200
+        dic = model_pred.to_dict()
+        #text_out = {dic}
+        return jsonify(dic), 200
     else:
         return jsonify({'message': 'MODEL_REPO cannot be found.'}), 200
